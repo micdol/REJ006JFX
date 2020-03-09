0ecbebdd97ae2e74af130653fb99f948446b4276
Michał Doliński <micdolinski@gmail.com>
2020-03-09 21:18:57 +0100

    Initial version of connection screen fxml view, already with dummy controller.
    Resolves #3

---
+++ src/main/java/com/dolinskm/rej006/views/ConnectionViewController.java
@@ -0,0 +1,45 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class ConnectionViewController {
+
+    @FXML
+    private ComboBox<?> cbxPorts;
+
+    @FXML
+    private Button btnRefreshPorts;
+
+    @FXML
+    private Button btnConnect;
+
+    @FXML
+    private Button btnDisconnect;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onConnectClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDisconnectClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRefreshPortsClicked(ActionEvent event) {
+
+    }
+
+}
