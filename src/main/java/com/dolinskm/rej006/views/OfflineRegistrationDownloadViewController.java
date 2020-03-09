f2a31dbd8a220853195e8b716051fec604ac6990
Michał Doliński <micdolinski@gmail.com>
2020-03-09 23:40:14 +0100

    Initial version of offline registration download screen fxml view, already with dummy controller.
    Resolves #6

---
+++ src/main/java/com/dolinskm/rej006/views/OfflineRegistrationDownloadViewController.java
@@ -0,0 +1,49 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Spinner;
+
+public class OfflineRegistrationDownloadViewController {
+
+    @FXML
+    private Spinner<?> spnRegistrationNumber;
+
+    @FXML
+    private Button btnDownload;
+
+    @FXML
+    private ProgressBar prgDownloadProgress;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnPlot;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDownloadClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPlotClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveClicked(ActionEvent event) {
+
+    }
+
+}
