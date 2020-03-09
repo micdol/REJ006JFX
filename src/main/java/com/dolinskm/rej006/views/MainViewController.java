c53580514de9fc4cef1b84007ee5f12daba88a4e
Michał Doliński <micdolinski@gmail.com>
2020-03-09 18:06:05 +0100

    Initial version of main screen fxml view, already with dummy controller.
    Resolves #1

---
+++ src/main/java/com/dolinskm/rej006/views/MainViewController.java
@@ -0,0 +1,65 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+public class MainViewController {
+
+    @FXML
+    private Button btnArchive;
+
+    @FXML
+    private Button btnAppSettings;
+
+    @FXML
+    private Button btnConnection;
+
+    @FXML
+    private Label lblPort;
+
+    @FXML
+    private CheckBox chkConnection;
+
+    @FXML
+    private Button btnDeviceInfo;
+
+    @FXML
+    private Button btnOnlineRegistrations;
+
+    @FXML
+    private Button btnOfflineRegistrations;
+
+    @FXML
+    void onAppSettingsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onArchiveClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onConnectionClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeviceInfoClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOfflineRegistrationsClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOnlineRegistrationsClicked(ActionEvent event) {
+
+    }
+
+}
