3cc94c1c4546693cd783176840ff5acc498fa024
Michał Doliński <micdolinski@gmail.com>
2020-03-09 23:03:22 +0100

    Initial version of device info screen fxml view, already with dummy controller.
    Resolves #4

---
+++ src/main/java/com/dolinskm/rej006/views/DeviceInfoViewController.java
@@ -0,0 +1,82 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class DeviceInfoViewController {
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtFirmware;
+
+    @FXML
+    private TextField txtFlashCapacity;
+
+    @FXML
+    private TextField txtRegistrationCount;
+
+    @FXML
+    private TextField txtBattery;
+
+    @FXML
+    private TextField txtStatus;
+
+    @FXML
+    private TextField txtSyncCode;
+
+    @FXML
+    private Button btnChangeID;
+
+    @FXML
+    private Button btnClearFlash;
+
+    @FXML
+    private Button btnChangeStatusSyncCode;
+
+    @FXML
+    private Button btnRefresh;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onChangeIDClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onChangeStatusSyncCode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClearFlashClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRefreshClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onResetClicked(ActionEvent event) {
+
+    }
+
+}
