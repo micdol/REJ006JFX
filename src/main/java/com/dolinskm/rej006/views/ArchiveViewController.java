ed7e18be743c94b47afd4c1110ad895bf61d05e9
Michał Doliński <micdolinski@gmail.com>
2020-03-09 21:09:35 +0100

    Initial version of archive screen fxml view, already with dummy controller.
    Resolves #2

---
+++ src/main/java/com/dolinskm/rej006/views/ArchiveViewController.java
@@ -0,0 +1,53 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+public class ArchiveViewController {
+
+    @FXML
+    private ComboBox<?> cbxDirectory;
+
+    @FXML
+    private Button btnBrowseDirectory;
+
+    @FXML
+    private TableView<?> tblRegistrations;
+
+    @FXML
+    private TextArea txtNotes;
+
+    @FXML
+    private Button btnSaveNotes;
+
+    @FXML
+    private Button btnShowPlot;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBrowseDirectoryClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSaveNotesClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShowPlotClicked(ActionEvent event) {
+
+    }
+
+}
