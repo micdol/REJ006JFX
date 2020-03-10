c00f913a467b13ef4070a4479b2f595ebab3c3f8
Michał Doliński <micdolinski@gmail.com>
2020-03-10 15:37:51 +0100

    Initial version of plot screen fxml view, already with dummy controller. No actual plot controls present, only dummy Regions.
    Resolves #8

---
+++ src/main/java/com/dolinskm/rej006/views/PlotViewController.java
@@ -0,0 +1,54 @@
+package com.dolinskm.rej006.views;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ToggleButton;
+
+public class PlotViewController {
+
+    @FXML
+    private ComboBox<?> cbxPlotMode;
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private Button btnStop;
+
+    @FXML
+    private ToggleButton tglCursorsX;
+
+    @FXML
+    private ToggleButton tglCursorsY;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCursorsXToggled(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCursorsYToggled(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStopClicked(ActionEvent event) {
+
+    }
+
+}
