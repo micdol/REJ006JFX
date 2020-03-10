b78765734cc5670e09d05c1d538891318ce2b283
Michał Doliński <micdolinski@gmail.com>
2020-03-10 15:25:12 +0100

    Initial version of online registrations screen fxml view, already with dummy controller.
    Resolves #7

---
+++ src/main/java/com/dolinskm/rej006/views/OnlineRegistrationViewController.java
@@ -0,0 +1,37 @@
+package com.dolinskm.rej006.views;
+
+import com.dolinskm.rej006.controls.SettingsGridController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class OnlineRegistrationViewController {
+
+    @FXML
+    private Button btnZeroing;
+
+    @FXML
+    private Button btnStartRegistration;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private SettingsGridController settingsGridController;
+    
+    @FXML
+    void onBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStartRegistrationClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onZeroingClicked(ActionEvent event) {
+
+    }
+
+}
