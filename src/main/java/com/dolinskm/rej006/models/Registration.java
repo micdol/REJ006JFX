541d9601fb7cc225ad515576fea1990963de6b1a
Michał Doliński <micdolinski@gmail.com>
2020-03-11 12:57:18 +0100

    Draft for #17, to be reviewed when initial versions of all models are finished.

---
+++ src/main/java/com/dolinskm/rej006/models/Registration.java
@@ -0,0 +1,74 @@
+package com.dolinskm.rej006.models;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalTime;
+
+public class Registration {
+    private final ObjectProperty<Settings> settings = new SimpleObjectProperty<>();
+    private final ObjectProperty<Mode> mode = new SimpleObjectProperty<>();
+    private final ObservableList<Byte> data = FXCollections.observableArrayList();
+    private final StringProperty name = new SimpleStringProperty();
+    private final ObjectProperty<LocalTime> date = new SimpleObjectProperty<>();
+
+    // region Properties Getters/Setters
+
+    public Settings getSettings() {
+        return settings.get();
+    }
+
+    public ObjectProperty<Settings> settingsProperty() {
+        return settings;
+    }
+
+    public void setSettings(Settings settings) {
+        this.settings.set(settings);
+    }
+
+    public Mode getMode() {
+        return mode.get();
+    }
+
+    public ObjectProperty<Mode> modeProperty() {
+        return mode;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode.set(mode);
+    }
+
+    public ObservableList<Byte> getData() {
+        return data;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public LocalTime getDate() {
+        return date.get();
+    }
+
+    public ObjectProperty<LocalTime> dateProperty() {
+        return date;
+    }
+
+    public void setDate(LocalTime date) {
+        this.date.set(date);
+    }
+
+    // endregion
+}
