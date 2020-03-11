28846ecf5df17f936d6a0c70cb22316018fd8856
Michał Doliński <micdolinski@gmail.com>
2020-03-11 12:48:33 +0100

    Draft for #16, to be reviewed when initial versions of all models are finished.

---
+++ src/main/java/com/dolinskm/rej006/models/Connection.java
@@ -0,0 +1,69 @@
+package com.dolinskm.rej006.models;
+
+import com.fazecast.jSerialComm.SerialPort;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Connection {
+
+    private final ObjectProperty<Device> device = new SimpleObjectProperty<>();
+    private final ObjectProperty<SerialPort> port = new SimpleObjectProperty<>();
+    private final BooleanProperty active = new SimpleBooleanProperty(false);
+    private final BooleanProperty busy = new SimpleBooleanProperty(false);
+
+    // region Properties Getters/Setters
+
+    public Device getDevice() {
+        return device.get();
+    }
+
+    public ObjectProperty<Device> deviceProperty() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device.set(device);
+    }
+
+    public SerialPort getPort() {
+        return port.get();
+    }
+
+    public ObjectProperty<SerialPort> portProperty() {
+        return port;
+    }
+
+    public void setPort(SerialPort port) {
+        this.port.set(port);
+    }
+
+    public boolean isActive() {
+        return active.get();
+    }
+
+    public BooleanProperty activeProperty() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active.set(active);
+    }
+
+    public boolean isBusy() {
+        return busy.get();
+    }
+
+    public BooleanProperty busyProperty() {
+        return busy;
+    }
+
+    public void setBusy(boolean busy) {
+        this.busy.set(busy);
+    }
+
+    // endregion
+}
